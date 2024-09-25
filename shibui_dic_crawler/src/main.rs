9c06009a9f063d6884f1bb44698152d7a1d16e7e
Do Tran <vivaladaman@gmail.com>
2024-09-25 22:43:50 +0700

    .

--- shibui_dic_crawler/src/main.rs
+++ shibui_dic_crawler/src/main.rs
@@ -19,7 +19,7 @@ async fn main() {
         }),
     );
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3001").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
